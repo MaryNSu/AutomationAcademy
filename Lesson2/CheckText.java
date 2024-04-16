4d34fb34d7a36f876d4d9abef067d6dea306de8b
MSUDENOC <readjustee@mail.ru>
2024-04-16 23:22:23 +0300

    initial commit for CheckText class

---
+++ Lesson2/CheckText.java
@@ -0,0 +1,13 @@
+package Lesson2;
+
+public class CheckText {
+    public static void main(String[] args) {
+        String text = "I love Java";
+        System.out.println("The length of the text string is: " + text.length());
+        char i = text.charAt(4);
+        System.out.println("The forth character in the text string is: " + i);
+        System.out.println("The index of the letter 'J' is: " + text.indexOf("J"));
+        System.out.println(text + " and QA Automation");
+        //boolean result =
+    }
+}
