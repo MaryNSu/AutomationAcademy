4e16b4f2918b130a32b1f92de12e9b69ac410c89
MSUDENOC <readjustee@mail.ru>
2024-04-17 20:18:45 +0300

    renamed and added new features

--- Lesson2/CheckTextTask5.java
+++ Lesson2/CheckTextTask5.java
@@ -1,9 +1,8 @@
 package Lesson2;
 
-public class CheckText {
+public class CheckTextTask5 {
     public static void main(String[] args) {
         String text = "I love Java";
-       // System.out.println("The length of the text string is: " + text.length());
         char i = text.charAt(4);
         System.out.println("The forth character in the text string is: " + i);
         System.out.println("The index of the letter 'J' is: " + text.indexOf("J"));
@@ -11,8 +10,8 @@ public class CheckText {
 
         if (text instanceof String){
             System.out.println("It is String");
-        }else {
-            System.out.println("It is not string");
+        }else{
+            System.out.println("It is not String");
         }
     }
 }
