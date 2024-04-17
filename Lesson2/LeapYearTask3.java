0eaa1d6e290ab4b5641cc89164cfbf7778be4ca7
MSUDENOC <readjustee@mail.ru>
2024-04-17 23:44:07 +0300

    new class was created

---
+++ Lesson2/LeapYearTask3.java
@@ -0,0 +1,22 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class LeapYearTask3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the year:");
+        int year = input.nextInt();
+        if (year % 4 == 0) {
+            if ((year % 100 == 0) && (year % 400 != 0))
+                System.out.println(year + " is not a leap year.");
+             else
+                System.out.println(year + " is a leap year.");
+            }else
+                System.out.println(year + " is not a leap year.");
+            }
+        }
+
+
+
+
