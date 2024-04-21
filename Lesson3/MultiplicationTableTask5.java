b37417f3ea517d5abd5bc7d6ecd906d557ca4668
MSUDENOC <readjustee@mail.ru>
2024-04-21 12:01:52 +0300

    new class MultiplicationTableTask5 was created

---
+++ Lesson3/MultiplicationTableTask5.java
@@ -0,0 +1,10 @@
+package Lesson3;
+
+public class MultiplicationTableTask5 {
+    public static void main(String[] args) {
+        int number = 3;
+        for (int i = 1; i <= 10; i++){
+            System.out.printf("%d * %d = %d%n", number, i, number * i);
+        }
+    }
+}
